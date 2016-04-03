55b30b173bd5a4de4bab9ea7eca0ff2719d51220
conscou <scoullisconstantina@gmail.com>
2016-04-03 11:47:04 -0400

    major frontend last resort push

--- index.js
+++ index.js
@@ -7,9 +7,27 @@ app.use(express.static(__dirname + '/'));
 
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
+app.get('/index', function(req, res) {
+    res.render('pages/index');
+});
+
+app.get('/login', function(req, res) {
+    res.render('pages/login');
+});
+
+app.get('/signup', function(req, res) {
     res.render('pages/signup');
 });
+
+
+app.get('/dashboard', function(req, res) {
+    res.render('pages/dashboard');
+});
+
+app.get('/search', function(req, res) {
+    res.render('pages/search');
+});
+
 
 app.listen(8080);
 console.log('8080 is the magic port! Hello World!');
